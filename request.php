0a41475ea600eec78d551e963205dacf83e05b97
inesdc <apple@MacBook-Pro-de-apple.local>
2014-07-05 19:26:28 +0200

    cmoit

--- request.php
+++ request.php
@@ -1,35 +1,23 @@
-function initialize(searchLat, searchLon) {
-  var mapOptions = {
-    center: new google.maps.LatLng(searchLat, searchLon),
-    zoom: 9,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
-  var marker = new google.maps.Marker();
-  google.maps.event.addListener(marker, "mouseover", function(event) {
-    // to get the geographical position:
-    var pos = marker.getPosition();
-    var lat = pos.lat();
-    var lng = pos.lng();
-    console.log('you are at ', lat, ' and ', lng);
-  });
-  return map;
-}
 
+
+<?php
+if(isset($_GET['fruta'])==true){
+ $fruta=$_GET['fruta'];
+ echo $fruta;}
+?>
+
+<script>
 function getFlickrPhotos(map, searchLat, searchLon) {
 
   var FLICKR_API_KEY = 'c8abcb2729a2b86f6c4a3492299cdeaf';
 
   var searchUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&";
-
+var fruta = "<php echo $fruta?>"
   var searchReqParams = {
     'api_key': FLICKR_API_KEY,
-    'tags': 'monument,statue,historical',
+    'tags': <?php echo $fruta?>,
     'has_geo': true,
-    'lat': searchLat,
-    'lon': searchLon,
-    //'place_id': place.place_id,
+    
     'accuracy': 11,
     'format': 'json',
     'safe_search': 1,
@@ -48,11 +36,10 @@ function getFlickrPhotos(map, searchLat, searchLon) {
     data: searchReqParams,
     success: function(data) {
       if (data.photos.photo.length > 0) {
-        getAndMarkPhotos(data.photos);
-        $('#warning').hide();
+        document.write(data.photos);
+       
       } else {
-        console.log(data.photos);
-        $('#warning').show();
+        
       }
       
     }
@@ -62,7 +49,7 @@ function getFlickrPhotos(map, searchLat, searchLon) {
     console.log('textStatus: ', textStatus, ' code: ', jqXHR.status);
   });
   
-
+/*
   function getAndMarkPhotos(photos) {
     var numPhotos = photos.photo.length;
     for(var i=0; i<numPhotos; i++) {
@@ -114,16 +101,4 @@ function getFlickrPhotos(map, searchLat, searchLon) {
     //map.setCenter(myLatLng);
   }
 }
-
-$(document).ready(function() {
-  $('#warning').hide();
-  $('#search').on('click', function() {
-    var searchLat = $('#lat').val();
-    var searchLon = $('#lon').val();
-    var googleMap = initialize(searchLat, searchLon);
-    getFlickrPhotos(googleMap, searchLat, searchLon);
-  });
-
-  $('#search').trigger('click');
-
-});
+</script>
